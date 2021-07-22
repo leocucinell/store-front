8984faaadca1b01c81134219227b34c99f8bb334
Leo Cucinell <leocucinell@gmail.com>
2021-07-21 20:48:07 -0700

    added skeletons to all GET routes

--- server.js
+++ server.js
@@ -30,7 +30,42 @@ const PORT = 4000;
 
 /* SECTION: Middleware */
 
+
+
 /* SECTION: Routes */
+app.get('/', (req, res) => {
+    res.redirect('/store');
+});
+
+/* NOTE: Index GET /store - Presentational */
+app.get('/store', (req, res) => {
+    res.send('Welcome to the store');
+});
+
+/* NOTE: New GET /store/new - Presentational form */
+app.get('/store/new', (req, res) => {
+    res.send('Here to make a new store item?');
+});
+
+/* NOTE: Create POST /store - Functional */
+
+/* NOTE: Show GET /store/:id - Presentational */
+app.get('/store/:id', (req, res) => {
+    res.send(`Showing data for item w/ id: ${req.params.id}`);
+});
+
+/* NOTE: Edit GET /store/:id/edit - Presentational form */
+app.get(`/store/:id/edit`, (req, res) => {
+    res.send(`Are you here to edit store item w/ id: ${req.params.id}?`);
+});
+
+/* NOTE: Update PUT /store/:id - Functional */
+
+/* NOTE: Destroy DELETE /store/:id - Functional */
+
+// 404 Page
+
+
 
 /* SECTION: Server bind */
 app.listen(PORT, () => {
