30087bd5da49d7e8ea3eb6bea24769aaf8901121
Leo Cucinell <leocucinell@gmail.com>
2021-07-21 20:28:07 -0700

    initial commit including dependences and notes

---
+++ server.js
@@ -0,0 +1,34 @@
+/* beginning notes:
+    - Data representation:
+        {
+            title: "String",
+            price: 10,
+            image: "String URL"
+        } ... { id: Generate }
+
+    - Tech Stack:
+        1) Node: Runtime Env
+        2) NPM: Package manager
+        3) Express: Server framework
+        4) ~~~ Mongoose: DB ~~~
+    
+    - dependences:
+        Express, ejs, method-override
+*/
+
+/* SECTION: External modules */
+const express = require("express");
+
+
+/* SECTION: Internal modules */
+
+/* SECTION: Instanced module */
+const app = express();
+
+/* SECTION: Server configuration */
+
+/* SECTION: Middleware */
+
+/* SECTION: Routes */
+
+/* SECTION: Server bind */
